8cbe3ebae63b0c68004271f564399cbab094db04
Mart van de Ven <m@type.hk>
2026-01-05 12:00:15 +0800

    Update .config/bat/config Update .config/hypr/scripts/toggle_special_ws.fish

--- home/dot_config/hypr/scripts/executable_toggle_special_ws.fish
+++ home/dot_config/hypr/scripts/executable_toggle_special_ws.fish
@@ -1,23 +1,17 @@
 #!/usr/bin/env fish
 
 # --- CONFIG ---
-set TARGET_MONITOR "HDMI-A-1"
+set TARGET_MONITOR "DP-1"
 set SPECIAL_WS "chat"
 
 set CURRENT_WIN (hyprctl activewindow -j | jq -r '.address')
 set CURRENT_MON (hyprctl monitors -j | jq -r '.[] | select(.focused) | .name')
-
-echo $CURRENT_WIN
-echo $CURRENT_MON
 
 if test "$CURRENT_MON" = "$TARGET_MONITOR"
     hyprctl dispatch togglespecialworkspace "$SPECIAL_WS"
     exit 0
 end
 
-
 hyprctl keyword cursor:no_warps true > /dev/null
-
 hyprctl --batch "dispatch focusmonitor $TARGET_MONITOR ; dispatch togglespecialworkspace $SPECIAL_WS ; dispatch focuswindow address:$CURRENT_WIN"
-
 hyprctl keyword cursor:no_warps false > /dev/null

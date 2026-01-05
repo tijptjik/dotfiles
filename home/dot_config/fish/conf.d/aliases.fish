82ff413a3a87448c8174ccd985411f933ada8586
Mart van de Ven <m@type.hk>
2026-01-05 11:58:02 +0800

    Add .config/bat/config Update .config/fish/conf.d/aliases.fish

--- home/dot_config/fish/conf.d/aliases.fish
+++ home/dot_config/fish/conf.d/aliases.fish
@@ -44,6 +44,17 @@ alias lrt='lsd -1Fcrt'
 alias lsdo='lsd -al | grep "^d"'
 
 #################################
+### CAT / FZF
+#################################
+
+alias cat='bat --paging=never'
+alias fzf='fzf --preview "bat --color=always --style=numbers --line-range=:500 {}"'
+
+# Highlighting --help messages
+abbr -a --position anywhere -- --help '--help | bat -plhelp'
+abbr -a --position anywhere -- -h '-h | bat -plhelp'
+
+#################################
 ### GREP / TAIL
 #################################
 

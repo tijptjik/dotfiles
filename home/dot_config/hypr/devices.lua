31ff3d04d61cf89c01348d818439a2ae40ecef7d
Mart van de Ven <m@type.hk>
2026-07-24 16:25:10 +0800

    refactor(hypr): Convert device config to Lua

---
+++ home/dot_config/hypr/devices.lua
@@ -0,0 +1,41 @@
+-- Sourced in ~/.config/hypr/hyprland.lua
+-- #######################
+-- HYPRLAND :: DEVICES
+-- #######################
+
+-- Author : Mart van de Ven
+-- Contact : https://type.hk
+
+-- #######################
+-- KEYBOARDS
+-- #######################
+
+hl.device({
+    name = "graystudio-space65",
+    numlock_by_default = true,
+    repeat_rate = 50,
+    repeat_delay = 320,
+})
+
+hl.device({
+    name = "xiudi-xd68",
+    numlock_by_default = true,
+    repeat_rate = 50,
+    repeat_delay = 320,
+})
+
+-- #######################
+-- MICE
+-- #######################
+
+hl.device({
+    name = "logitech-mx-vertical",
+    sensitivity = 0.3,
+    -- accel_profile = "adaptive",
+})
+
+hl.device({
+    name = "logitech-mx-vertical-1",
+    sensitivity = 0.3,
+    -- accel_profile = "adaptive",
+})

65f09de856f543e9ba40eea605f895e4f93c416e
Mart van de Ven <m@type.hk>
2026-07-24 16:21:23 +0800

    refactor(hypr): migrate bindings configuration to Lua

---
+++ home/dot_config/hypr/plugins.lua
@@ -0,0 +1,9 @@
+-- Sourced in ~/.config/hypr/hyprland.lua
+-- #######################
+-- HYPRLAND :: PLUGINS
+-- #######################
+
+-- Author : Mart van de Ven
+-- Contact : https://type.hk
+
+-- See https://hypr.land/plugins/
